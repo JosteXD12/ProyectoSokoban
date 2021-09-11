e41c8e0d77ede87b85acce8882b33ecdf0cf66f8
Josue cortes <jostecortes12@gmail.com>
2021-09-11 14:43:06 -0600

    Actualizacion 0.1

--- ProyectoSokoban/main.h
+++ ProyectoSokoban/main.h
@@ -1,5 +1,14 @@
-#pragma once
-class main
-{
-};
+#include "SFML/Graphics.hpp"
+#include "iostream"
 
+using namespace sf;
+using namespace std;
+
+int main() {
+	bool gameover = false;
+	RenderWindow ventana(VideoMode(800, 600, 64), "mi Ventana");
+	while (!gameover) {
+		ventana.display();
+	}
+	return 0; 
+}
