2c7bca1d706cad9d3c84afcccef362ec0cfa6437
Josue cortes <jostecortes12@gmail.com>
2021-09-11 16:54:53 -0600

    0.8

--- ProyectoSokoban/main.h
+++ ProyectoSokoban/main.h
@@ -7,9 +7,11 @@ using namespace std;
 int main() {
 	bool gameover = false;
 	RenderWindow ventana(VideoMode(800, 600, 64), "mi Ventana");
+	cout << "hello word"<<endl;
+	cout << "My name is Cruella";
 	while (!gameover) {
 		ventana.display();
-		cout << "hello word";
+		
 		//dghauiosdhpaiopsd
 		//DASDASDAS
 		//dadsad
